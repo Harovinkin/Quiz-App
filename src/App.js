0ffefb4fafba9b08a392bd13c41d1cb2361a01fd
Harovinkin <kharovinkin@gmail.com>
2020-05-27 20:05:14 +0300

    plugged_switch_route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import Layout from './hoc/Layout/Layout';
+import React from "react";
+import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
+import { Switch, Route } from "react-router-dom";
+import Auth from "./containers/Auth/Auth";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
 
 function App() {
   return (
-    <Layout />
+    <Layout>
+      <Switch>
+        <Route to="/auth" component={Auth} />
+        <Route to="/quiz-creator" component={QuizCreator} />
+        <Route to="/quiz/:id" component={Quiz} />
+        <Route to="/" component={QuizList} />
+      </Switch>
+    </Layout>
   );
 }
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
-import classes from './Layout.module.css';
-import Quiz from '../../containers/Quiz/Quiz';
-import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
-import Drawer from '../../components/Navigation/Drawer/Drawer';
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
+import Quiz from "../../containers/Quiz/Quiz";
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
   state = {
     isMenuOpen: false,
-  }
+  };
 
   toggleMenuHandler = () => {
     this.setState({
-      isMenuOpen: !this.state.isMenuOpen
-    })
-  }
+      isMenuOpen: !this.state.isMenuOpen,
+    });
+  };
 
   menuCloseHandler = () => {
     this.setState({
-      isMenuOpen: false
-    })
-  }
+      isMenuOpen: false,
+    });
+  };
 
   render() {
     return (
       <div className={classes.Layout}>
-        <Drawer 
+        <Drawer
           isOpen={this.state.isMenuOpen}
           onClose={this.menuCloseHandler}
         />
-        <MenuToggle 
+        <MenuToggle
           isOpen={this.state.isMenuOpen}
           onToggle={this.toggleMenuHandler}
         />
-        <main>
-          <Quiz />
-        </main>
+        <main>{this.props.children}</main>
       </div>
     );
   }
