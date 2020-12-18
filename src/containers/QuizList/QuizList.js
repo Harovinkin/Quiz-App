1325a22739ed24a242cc2f4305fa3dce8739b67a
Harovinkin <kharovinkin@gmail.com>
2020-12-18 13:47:05 +0200

    quizes GET

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
 import classes from "./QuizList.module.css";
 import { NavLink } from "react-router-dom";
-// import axios from "axios";
+import Axios from "axios";
 
 export default class QuizList extends Component {
+  state = {
+    quizes: []
+  }
+
   renderQuizList() {
-    return [1, 2, 3].map((quiz, idx) => (
-      <li key={idx}>
-        <NavLink to={`/quiz/${quiz}`}>Test {quiz}</NavLink>
+    return this.state.quizes.map(quiz => (
+      <li key={quiz.id}>
+        <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
       </li>
     ));
   }
 
-  // componentDidMount() {
-  //   axios.get('https://react-quiz-46e2a.firebaseio.com/quiz.json')
-  //   .then(response => {
-  //     console.log(response)
-  //   })
-  // }
+  async componentDidMount() {
+    try {
+      const response = await Axios.get('https://react-quiz-46e2a.firebaseio.com/quizes.json')
+      
+      const quizes = []
+      
+      Object.keys(response.data).forEach((key, idx) => {
+        quizes.push({
+          id: key,
+          name: `Quiz №${idx + 1}`
+        })
+      })
+
+      this.setState({
+        quizes
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   render() {
+    
     return (
       <div className={classes.QuizList}>
         <h1>Quiz List</h1>
